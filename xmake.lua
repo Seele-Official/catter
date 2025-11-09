0a1c907f9a94326e4ed1f603d1b828136c08866a
star9029 <hengxings783@gmail.com>
2025-11-09 23:41:08 +0800

    build: improve xmake (#6)

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,5 @@
 set_project("catter")
+
 add_rules("mode.debug", "mode.release")
 set_allowedplats("windows", "linux")
 
@@ -6,27 +7,25 @@ set_languages("c++23")
 
 option("dev", {default = true})
 if has_config("dev") then
-   add_rules("plugin.compile_commands.autoupdate", {outputdir = "build", lsp = "clangd"})
+    set_policy("build.ccache", true)
+    add_rules("plugin.compile_commands.autoupdate", {outputdir = "build", lsp = "clangd"})
 end
 
 if is_plat("windows") then
     add_requires("microsoft-detours")
 end
 
-if is_plat("windows") then
-    target("catter-hook64")
-        set_kind("shared")
-        add_includedirs("src")
-        add_files("src/hook/windows/hook.cpp")
-        add_syslinks("user32")
-        add_packages("microsoft-detours")
-end
-
 if is_plat("linux") then
-  includes("src/hook/linux")
+    includes("src/hook/linux")
 end
 
-
+target("catter-hook64")
+    set_kind("shared")
+    set_enabled(is_plat("windows"))
+    add_includedirs("src")
+    add_files("src/hook/windows/hook.cpp")
+    add_syslinks("user32")
+    add_packages("microsoft-detours")
 
 target("catter")
     set_kind("binary")
@@ -34,8 +33,10 @@ target("catter")
     add_files("src/main.cpp")
     if is_plat("windows") then
         add_files("src/hook/windows/impl.cpp")
-        add_packages("microsoft-detours")
-    end
-    if is_plat("linux") then
+    elseif is_plat("linux") then
         add_files("src/hook/linux/*.cc")
+    end
+
+    if is_plat("windows") then
+        add_packages("microsoft-detours")
     end
