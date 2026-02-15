87695b6f8d4318db75268a371e559d08a1c87d0e
seele <seele@gmail.com>
2026-02-16 00:02:01 +0800

    refactor: streamline project configuration retrieval in integration tests

--- tests/integration/lit.cfg.py
+++ tests/integration/lit.cfg.py
@@ -28,38 +28,31 @@ config.name = "Catter Integration Test"
 config.test_format = lit.formats.ShTest(True)
 config.suffixes = [".test", ".cc"]
 
-project_root = run_with_json("xmake show --json")["project"]["projectdir"]
+project_config = run_with_json("xmake show --json")
+
+project_root = project_config["project"]["projectdir"]
+project_mode = project_config["project"]["mode"]
 
 config.test_source_root = os.path.normpath(f"{project_root}/tests/integration/test")
 config.test_exec_root = os.path.normpath(f"{project_root}/build/lit-tests")
 
 hook_config = run_with_json("xmake show -t it-catter-hook --json")
-proxy_config = run_with_json("xmake show -t it-catter-proxy --json")
-
 hook_path = os.path.join(project_root, hook_config["targetfile"])
+
+proxy_config = run_with_json("xmake show -t it-catter-proxy --json")
 proxy_path = os.path.join(project_root, proxy_config["targetfile"])
 
 match platform.system():
     case "Windows":
         config.test_format = lit.formats.ShTest(False)
     case "Linux":
-        mode = run_with_json("xmake show --json")["project"]["mode"]
-
-        if mode == "debug":
-            hook_compiler = hook_config["compilers"][0]["program"]
-            if "g++" in hook_compiler:
-                hook_asan_path = run(
-                    f"{hook_compiler} -print-file-name=libasan.so"
-                ).strip()
+        if project_mode == "debug":
+            compiler = run_with_json("xmake show -t catter --json")["compilers"][0][
+                "program"
+            ]
+            if "g++" in compiler and "clang" not in compiler:
+                hook_asan_path = run(f"{compiler} -print-file-name=libasan.so").strip()
                 hook_path = f"LD_PRELOAD={hook_asan_path} {hook_path}"
-
-            proxy_compiler = proxy_config["compilers"][0]["program"]
-            if "g++" in proxy_compiler:
-                proxy_asan_path = run(
-                    f"{proxy_compiler} -print-file-name=libasan.so"
-                ).strip()
-                proxy_path = f"LD_PRELOAD={proxy_asan_path} {proxy_path}"
-
 
 config.substitutions.append(("%it_catter_hook", hook_path))
 config.substitutions.append(("%it_catter_proxy", proxy_path))
