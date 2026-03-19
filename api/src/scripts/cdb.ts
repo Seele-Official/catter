f0d3706efe086a61c728fe5118b959d86328b781
seele <seele@gmail.com>
2026-03-19 23:55:26 +0800

    feat: enhance CDB command handling and parsing logic

--- api/src/scripts/cdb.ts
+++ api/src/scripts/cdb.ts
@@ -2,23 +2,66 @@ import * as service from "../service.js";
 
 import * as io from "../io.js";
 import * as fs from "../fs.js";
+import * as option from "../option/index.js";
 import { identify_compiler, Compiler } from "catter-c";
+
+type CDBItem = {
+  directory: string;
+  file: string;
+  command: string;
+  arguments?: string[];
+  output?: string;
+};
 
 export class CDB implements service.CatterService {
   save_path: string;
-  commandArray: Array<[Compiler, string]> = [];
+  commandArray: Array<[Compiler, service.CommandData]> = [];
 
   constructor(save_path: string) {
     this.save_path = save_path;
   }
 
+  parse(table: option.OptionTable, args: string[]): option.OptionItem[] {
+    const parsed: option.OptionItem[] = [];
+    option.parse(table, args, (parseRes) => {
+      if (typeof parseRes === "string") {
+        throw new Error(`Parsing error: ${parseRes}`);
+      }
+      parsed.push(parseRes);
+      return true;
+    });
+    return parsed;
+  }
+
   onStart(config: service.CatterConfig): service.CatterConfig {
     return config;
   }
+
   onFinish() {
+    fs.removeAll(this.save_path);
     fs.createFile(this.save_path);
+    const cdb: CDBItem[] = [];
+
+    for (const [compiler, command] of this.commandArray) {
+      switch (compiler) {
+        case "clang":
+          const parsed = this.parse(compiler, command.argv.slice(1));
+          for (const item of parsed) {
+            if (item.id === option.ClangID.ID_INPUT) {
+              cdb.push({
+                directory: command.cwd,
+                file: item.key,
+                command: command.argv.join(" "),
+              });
+            }
+          }
+        default:
+          throw new Error(`Unsupported compiler: ${compiler}`);
+      }
+    }
+
     io.TextFileStream.with(this.save_path, "ascii", (stream) => {
-      stream.write(JSON.stringify(this.commandArray, null, 2));
+      stream.write(JSON.stringify(cdb, null, 2));
     });
   }
 
@@ -31,7 +74,7 @@ export class CDB implements service.CatterService {
     } else {
       const compiler = identify_compiler(data.exe);
       if (compiler !== "unknown") {
-        this.commandArray.push([compiler, data.argv.join(" ")]);
+        this.commandArray.push([compiler, data]);
       }
     }
     return {
